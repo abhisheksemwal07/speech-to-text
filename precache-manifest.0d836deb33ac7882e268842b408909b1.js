21c8a122bbba95874a68c365ed46dfc08e6c7454
abhisheksemwal07 <abhisheksemwal619@gmail.com>
2024-09-09 15:27:05 +0530

    Updates

---
+++ precache-manifest.0d836deb33ac7882e268842b408909b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85f59ece17d409f24280efe7100ff8c",
+    "url": "/speech-to-text/index.html"
+  },
+  {
+    "revision": "0cd63aaaaf508d890c0b",
+    "url": "/speech-to-text/static/css/main.7fa6173a.chunk.css"
+  },
+  {
+    "revision": "c49e47dee42dce766b7a",
+    "url": "/speech-to-text/static/js/2.a52ba83b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/speech-to-text/static/js/2.a52ba83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e8d8fc8f55b8c91be8",
+    "url": "/speech-to-text/static/js/3.0b4df0d0.chunk.js"
+  },
+  {
+    "revision": "0cd63aaaaf508d890c0b",
+    "url": "/speech-to-text/static/js/main.b3d3e8b6.chunk.js"
+  },
+  {
+    "revision": "e0595698d4e724abe05d",
+    "url": "/speech-to-text/static/js/runtime-main.a92b7468.js"
+  }
+]);
